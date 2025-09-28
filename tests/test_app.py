fdaf8c98a9ee6d737183b64dcd7eab29c2030eb0
Sven Fraeys <sven.fraeys@gmail.com>
2025-09-28 22:07:17 +0200

    cleaned up models and added basic unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,21 @@
-import sfasset_service.main
+from fastapi.testclient import TestClient
+
+from sfasset_service.main import app
+
+
+client = TestClient(app)
 
 
 def test_app():
-    assert sfasset_service.main.app
+    assert app
+
+
+def test_create_project():
+    response = client.post(
+        "/projects/",
+        json={"name": "test"},
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "test"
+    assert "id" in data
